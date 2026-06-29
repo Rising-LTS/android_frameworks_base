1dd9c4dce753ba745011164561cb827734aa38e1
OliverSyx <173170056+OliverSyx@users.noreply.github.com>
2026-06-30 00:05:47 +0700

    Revert "services: Implement activity boost optimization"
    This reverts commit 6444fc7c0b662082d0c9387f139f686846cb21b0.

--- services/core/java/com/android/server/wm/ActivityRecord.java
+++ services/core/java/com/android/server/wm/ActivityRecord.java
@@ -291,7 +291,6 @@ import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
 import android.gui.DropInputMode;
 import android.hardware.HardwareBuffer;
-import android.hardware.power.Mode;
 import android.net.Uri;
 import android.os.Binder;
 import android.os.Build;
@@ -300,7 +299,6 @@ import android.os.Debug;
 import android.os.IBinder;
 import android.os.IRemoteCallback;
 import android.os.PersistableBundle;
-import android.os.PowerManagerInternal;
 import android.os.Process;
 import android.os.RemoteException;
 import android.os.SystemClock;
@@ -6052,13 +6050,12 @@ public final class ActivityRecord extends WindowToken implements WindowManagerSe
                 Slog.v(TAG_VISIBILITY, "Start visible activity, " + this);
             }
             setState(STARTED, "makeActiveIfNeeded");
-            setActivityBoost(true);
+
             try {
                 mAtmService.getLifecycleManager().scheduleTransaction(app.getThread(), token,
                         StartActivityItem.obtain(takeOptions()));
             } catch (Exception e) {
                 Slog.w(TAG, "Exception thrown sending start: " + intent.getComponent(), e);
-                setActivityBoost(false);
             }
             // The activity may be waiting for stop, but that is no longer appropriate if we are
             // starting the activity again
@@ -6687,16 +6684,8 @@ public final class ActivityRecord extends WindowToken implements WindowManagerSe
         }
     }
 
-    protected void setActivityBoost(boolean enable) {
-        PowerManagerInternal mPowerManagerInternal = LocalServices.getService(PowerManagerInternal.class);
-        if (mPowerManagerInternal != null) {
-            mPowerManagerInternal.setPowerMode(Mode.LAUNCH, enable);
-        }
-    }
-
     /** Called when the windows associated app window container are drawn. */
     private void onWindowsDrawn(long timestampNs) {
-        setActivityBoost(false);
         final TransitionInfoSnapshot info = mTaskSupervisor
                 .getActivityMetricsLogger().notifyWindowsDrawn(this, timestampNs);
         final boolean validInfo = info != null;
