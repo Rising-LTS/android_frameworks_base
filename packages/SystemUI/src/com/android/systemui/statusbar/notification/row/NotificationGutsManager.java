72ad5333e503ad80a9e4ee8c1e9716904f3d13a5
OliverSyx <173170056+OliverSyx@users.noreply.github.com>
2026-07-02 21:45:02 +0700

    Revert "Prevent long press on profile notifs when locked"
    This reverts commit 95631b96650bd77f3bc65bc10a5e33a8b3db19ec.

--- packages/SystemUI/src/com/android/systemui/statusbar/notification/row/NotificationGutsManager.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/notification/row/NotificationGutsManager.java
@@ -586,10 +586,6 @@ public class NotificationGutsManager implements NotifGutsViewManager {
         }
 
         final ExpandableNotificationRow row = (ExpandableNotificationRow) view;
-        if (affectedByWorkProfileLock(row)) {
-            return false;
-        }
-
         if (row.isNotificationRowLongClickable()) {
             view.performHapticFeedback(HapticFeedbackConstants.LONG_PRESS);
         }
@@ -649,12 +645,6 @@ public class NotificationGutsManager implements NotifGutsViewManager {
         };
         guts.post(mOpenRunnable);
         return true;
-    }
-
-    boolean affectedByWorkProfileLock(ExpandableNotificationRow row) {
-        int userId = row.getEntry().getSbn().getNormalizedUserId();
-        return mUserManager.isManagedProfile(userId)
-                && mLockscreenUserManager.isLockscreenPublicMode(userId);
     }
 
     /**
